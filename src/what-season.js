ef136f08f29502acaeaf8fbd5302e67046dc5f4a
elygo <el.uzb93@gmail.com>
2020-10-28 01:49:54 +0500

    feat: add solution for What season?
    Problem is solved via access to a month value.

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  let yearTime;
+  let dateFull = date;
+  if (dateFull === null || dateFull === undefined) {
+    return 'Unable to determine the time of year!'; 
+  }  else if (typeof dateFull.getMonth !== 'function' || Object.prototype.toString.call(dateFull) !== '[object Date]') {
+    throw new Error('Invalid argument'); 
+  } else {
+    let dateTimenum = dateFull.getMonth();
+    switch(true) {
+      case (dateTimenum === 11):
+          yearTime = 'winter';
+          break;
+      case (0 <= dateTimenum && dateTimenum <= 1):
+          yearTime = 'winter';
+          break;
+      case (2 <= dateTimenum && dateTimenum <= 4):
+          yearTime = 'spring';
+          break;
+      case (5 <= dateTimenum && dateTimenum <= 7):
+          yearTime = 'summer';
+          break;
+      case (8 <= dateTimenum && dateTimenum <= 10):
+          yearTime = 'autumn';
+          break;
+      default:
+          break;
+    }
+    
+    return yearTime;
+
+  }
 };
