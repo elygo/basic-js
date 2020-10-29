e2de2051213b2490076ead2c9172aaa61dde825d
elygo <el.uzb93@gmail.com>
2020-10-30 01:12:21 +0500

    feat: add hanoi and transform array functions
    Hanoi: pow (2, n) - 1 and 3600/speed * turns
    Transform: using if conditions checking i-2, i-1, i and i+1 indeces of array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if ( arr === undefined || arr === null || arr.length === 0){
+    throw new CustomError('Error');
+  } else {
+    let newArray = [];
+    for(var i=0; i < arr.length; i++){
+      if(arr[i] === '--discard-prev'){
+        if (i === 0) {
+
+        } else if (i > 1 && arr[i-2] === '--discard-next') {
+
+        } else {
+          newArray.pop();
+        }
+      } else if(arr[i] === '--double-prev'){
+        if (i === 0) {
+
+        } else if (i > 1 && arr[i-2] === '--discard-next') {
+
+        } else {
+          newArray.push(arr[i-1]);
+        }
+      } else if(i > 0 && arr[i-1] === '--discard-next'){
+        
+      } else if (arr[i] === '--discard-next') {
+
+      } else if (arr[i] === '--double-next') {
+        if (arr[arr.length-1] === arr[i]) {
+
+        } else {
+          newArray.push(arr[i+1]);
+        }
+      } else {
+        newArray.push(arr[i]);
+      }
+    }
+    return newArray;
+  }
+  
 };
